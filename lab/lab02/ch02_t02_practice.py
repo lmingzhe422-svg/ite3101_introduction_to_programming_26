f6e6b77bc6c8dd12e84f8e476f32a05c8dcf44f0
lmingzhe422-svg <lmingzhe422@gmail.com>
2026-09-17 07:41:47 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,5 +1,6 @@
 # Assign your variables below, each on its own line!
-cawsar = ""
+cawsar = "Grahan"
+
 
 # Put your variables above this line, and uncomment the following 3 line of codes.
 print(caesar)
