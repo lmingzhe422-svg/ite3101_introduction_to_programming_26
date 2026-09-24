256610f699d7118a94e14c7d8a0d9cf239d073f4
lmingzhe422-svg <lmingzhe422@gmail.com>
2026-09-24 08:29:22 +0000

    lab/lab05/ch05_t07_word_up.py

--- lab/lab05/ch05_t07_word_up.py
+++ lab/lab05/ch05_t07_word_up.py
@@ -3,6 +3,7 @@ pyg = 'ay'
 original = input('Enter a word:')
 
 if len(original) > 0 and original.isalpha():
-    word = original.lower
+    word = original.lower()
+    first = word[0]
 else:
     print('empty')
