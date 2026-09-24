f578b17c7d2cbb0e9ac0af80f76132d1be381e13
lmingzhe422-svg <lmingzhe422@gmail.com>
2026-09-24 07:24:24 +0000

    [skip actions] lab/lab04/ch04_t04_how_tables_have_turned.py

--- lab/lab04/ch04_t04_how_tables_have_turned.py
+++ lab/lab04/ch04_t04_how_tables_have_turned.py
@@ -7,10 +7,10 @@ bool_one = 3 < 5  # We already did this one for you!
 bool_two = 3 > 5
 
 # Make me true!
-bool_three = None
+bool_three = 3 != 5
 
 # Make me false!
-bool_four = None
+bool_four = 3 == 5
 
 # Make me true!
 bool_five = None
