84c25dcaca7a8139f819f7fcfc347c788e9f71e6
lmingzhe422-svg <lmingzhe422@gmail.com>
2026-09-24 07:30:57 +0000

    lab/lab04/ch04_t07_or.py

--- lab/lab04/ch04_t07_or.py
+++ lab/lab04/ch04_t07_or.py
@@ -1,9 +1,9 @@
-bool_one = None
+bool_one = True
 
-bool_two = None
+bool_two = True
 
-bool_three = None
+bool_three = False
 
-bool_four = None
+bool_four = True
 
-bool_five = None
+bool_five = False
