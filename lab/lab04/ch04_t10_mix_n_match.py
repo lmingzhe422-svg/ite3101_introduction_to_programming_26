bb98a85ce9da3e9cbd05b3760c7182f1f7efcc80
lmingzhe422-svg <lmingzhe422@gmail.com>
2026-09-24 07:40:24 +0000

    [skip actions] lab/lab04/ch04_t10_mix_n_match.py

--- lab/lab04/ch04_t10_mix_n_match.py
+++ lab/lab04/ch04_t10_mix_n_match.py
@@ -7,10 +7,10 @@ bool_one = (2 <= 2) and "Alpha" == "Bravo"  # We did this one for you!
 bool_two = (2 <= 2) and "Alpha" == "Bravo"
 
 # Make me false!
-bool_three = Non
+bool_three = (2 <= 2) and "Alpha" == "Bravo"
 
 # Make me true!
-bool_four = None
+bool_four = (2 <= 2) and "Alpha" == "Bravo"
 
 # Make me true!
 bool_five = None
